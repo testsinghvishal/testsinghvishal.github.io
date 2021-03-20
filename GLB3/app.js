378b3a27ecccacc3576217a3ef4bbb2ac5cca2f3
testsinghvishal <78307367+testsinghvishal@users.noreply.github.com>
2021-03-20 19:54:00 +0530

    Update app.js

--- GLB3/app.js
+++ GLB3/app.js
@@ -19,7 +19,7 @@ function init() {
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 50, 150);
+  camera.position.set(0, 20, 150);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
